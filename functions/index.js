ad87195bf2088cd80571b2d71a552c8cf2772f40
Devon Ward <devoncward@gmail.com>
2019-06-06 14:24:19 -0600

    delete working

--- functions/index.js
+++ functions/index.js
@@ -15,46 +15,49 @@ var itemsRef = db.collection('items');
 
 // Get a list of all of the items in the museum.
 app.get('/api/items', async (req, res) => {
-    try{
-        let querySnapshot = await itemsRef.get();
-        res.send(querySnapshot.docs.map(doc => doc.data()));
-    }catch(err){
-        res.sendStatus(500);
-    }
+  try{
+    let querySnapshot = await itemsRef.get();
+    res.send(querySnapshot.docs.map(doc => doc.data()));
+  }catch(err){
+    res.sendStatus(500);
+  }
 });
 
 app.post('/api/items', async (req, res) => {
-    try {
-        let querySnapshot = await itemsRef.get();
-        let numRecords = querySnapshot.docs.length;
-        let item = {
-            id: numRecords + 1,
-            title: req.body.title,
-            path: req.body.path
-        };
-        console.log("idem: ", item);
-        itemsRef.doc(item.id.toString()).set(item);
-        res.send(item);
-      } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-      }
+  try {
+    let querySnapshot = await itemsRef.get();
+    let numRecords = querySnapshot.docs.length;
+    let item = {
+      id: numRecords + 1,
+      title: req.body.title,
+      path: req.body.path
+    };
+    console.log("idem: ", item);
+    itemsRef.doc(item.id.toString()).set(item);
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 app.delete('/api/items/:id', async (req, res) => {
-    try {
-        let id = req.params.id.toString();
-        var documentToDelete = itemsRef.doc(id);
-        if(!doc.exists) {
-          res.status(404).send("Sorry that item doesn't exist");
-          return;
-        } else {
-          documentToDelete.delete();
-          res.sendStatus(200);
-          return;
-        }
-    } catch (error) {
-      res.status(500).send("Error deleting document: ", error);
+  console.log("Something is happening-----------------");
+  try {
+    let id = req.params.id.toString();
+    console.log("I reached here", id);
+    var documentToDelete = itemsRef.doc(id);
+    var doc = await documentToDelete.get();
+    if(!doc.exists) {
+      res.status(404).send("Sorry that item doesn't exist");
+      return;
+    } else {
+      documentToDelete.delete();
+      res.sendStatus(200);
+      return;
     }
+  } catch (error) {
+    res.status(500).send("Error deleting document: ", error);
+  }
 });
 exports.app = functions.https.onRequest(app);
